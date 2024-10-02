b67ce5aec1df2906f4a435acd16e18a27c086e9c
Calvinbullock <calvinnbullock@gmail.com>
2024-10-02 08:04:02 -0600

    linted and formated

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,6 @@
-import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
-import { findProductById } from "./productData.mjs";
+import { getParam } from "./utils.mjs";
 import productDetails from "./productDetails.mjs";
 
-
-const productId = getParam('product');
+const productId = getParam("product");
 
 productDetails(productId);
