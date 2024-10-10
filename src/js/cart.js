083f6a82adaadc239cad1fe8a77a870ded4c00c7
Sarah Glenn <sarahadaline@gmail.com>
2024-10-10 10:14:42 +0200

    sg added icons and functionality to remove items from the cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,4 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
@@ -19,10 +19,26 @@ function cartItemTemplate(item) {
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
+  <button class="cart-remove__button" data-id=${item.Id}>❌</button>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
 
   return newItem;
+}
+
+document.addEventListener("click", event => {
+    if (event.target.matches(".cart-remove__button")) {
+        removeFromCart(event.target.getAttribute("data-id"));
+    }
+});
+
+function removeFromCart(id) {
+  let cartItems = getLocalStorage("so-cart");
+  const index = cartItems.findIndex(item => item.Id === id);
+  cartItems.splice(index, 1);
+  setLocalStorage("so-cart", cartItems);
+  renderCartContents();
+  showTotal();
 }
 
 function calculateCartTotal() {
@@ -36,9 +52,13 @@ function calculateCartTotal() {
 }
 
 function showTotal() {
-  document.querySelector(".cart-footer").style.display = "block";
-  const total = document.querySelector(".cart-footer p");
-  total.innerHTML = `Total: $${calculateCartTotal()}`;
+  if (calculateCartTotal() > 0) {
+    document.querySelector(".cart-footer").style.display = "block";
+    const total = document.querySelector(".cart-footer p");
+    total.innerHTML = `Total: $${calculateCartTotal()}`;
+  } else {
+    document.querySelector(".cart-footer").style.display = "none";
+  }
 }
 
 if (getLocalStorage("so-cart")) {
