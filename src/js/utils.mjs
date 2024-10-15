625ab4728fd1e4adea16cbd148e8f07e62f6242a
Calebjorgensen <jjorgyc@gmail.com>
2024-10-15 12:25:40 -0700

    Team week 4

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -34,3 +34,50 @@ export function createErrorMsg(msg) {
             <p class="error-msg">${msg}<p/>
          <div/>`;
 }
+
+
+export async function renderWithTemplate( templateFn, parentElement, data, callback, position = "afterbegin", clear = true) {
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+
+  const htmlString = await templateFn(data);
+
+  parentElement.insertAdjacentHTML(position, htmlString);
+  if (callback) {
+    callback(data);
+  }
+
+}
+
+
+function loadTemplate(path) {
+  return async function() {
+    const res = await fetch(path);
+    if(res.ok) {
+      const html = await res.text();
+      return html;
+   }
+  };
+}
+
+// const headerTemplateFn = loadTemplate("/partials/header.html");
+// const footerTemplateFn = loadTemplate("/partials/footer.html");
+
+export async function loadHeaderFooter() {
+  const headerTemplateFn = loadTemplate("/partials/header.html");
+  const footerTemplateFn = loadTemplate("/partials/footer.html"); 
+
+  const headerHTML = await headerTemplateFn();
+  const footerHTML = await footerTemplateFn();
+
+  const header = document.getElementById('main-header');
+  const footer = document.getElementById('main-footer');
+
+  renderWithTemplate(headerTemplateFn, header);
+  renderWithTemplate(footerTemplateFn, footer);
+
+
+
+}
+ 
