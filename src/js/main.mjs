8dff184527065fe331a49fd726a50eaeb2c30d9d
Sarah Glenn <sarahadaline@gmail.com>
2024-10-25 22:43:29 +0200

    newsletter sign-up box and styles

--- src/js/main.mjs
+++ src/js/main.mjs
@@ -1,9 +1,9 @@
-import productList from "./productList.mjs"
+// import productList from "./productList.mjs"
 
 import { loadHeaderFooter, waitForCartCount } from "./utils.mjs"
 
 // Default Sort By
-let sortBy = "Name A - Z";
+// let sortBy = "Name A - Z";
 
 loadHeaderFooter();
 
@@ -11,11 +11,48 @@ loadHeaderFooter();
 
 waitForCartCount();
 
-const dropdown = document.getElementById("sort-by");
+// const dropdown = document.getElementById("sort-by");
 
 // dropdown.addEventListener("change", () => {
 //   sortBy = dropdown.value;
 //   productList(sortBy, ".product-list")
 // });
 
+// Newsletter sign-up dialog elements
+const subscribeBtn = document.querySelector(".subscribe");
+const signupBox = document.querySelector(".newsletter dialog");
+const closeBtn = document.querySelector(".newsletter dialog button");
+const submitBtn = document.querySelector("#submitBtn");
 
+// Function to dismiss the dialog when clicking anywhere outside it
+const dismiss = ({target:dialogBox}) => {
+    if (dialogBox.nodeName === "DIALOG")
+        dialogBox.close("dismiss")
+}
+
+// Open the newsletter sign-up dialog
+subscribeBtn.addEventListener("click", () => {
+  signupBox.showModal();
+  signupBox.addEventListener("click", dismiss);
+});
+
+closeBtn.addEventListener("click", () => signupBox.close());
+
+// Display a thank you message to confirm subscription
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const form = signupBox.querySelector("form");
+  if (form.checkValidity()) {
+
+    const thanksMsg = document.createElement("p");
+    thanksMsg.textContent = `${form.name.value}, thanks for subscribing to the Sleep Outside Newsletter!`;
+  
+    //replace form with the thanks message
+    form.replaceWith(thanksMsg);
+    // close the dialog after 3 seconds
+    setTimeout(() => signupBox.close(), 3000);
+  } else {
+    form.reportValidity();
+  }
+});
