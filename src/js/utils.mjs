74fdc46fae363269ddab1e0a6cc5b49164994c4c
Calvinbullock <calvinnbullock@gmail.com>
2024-11-07 13:13:24 -0700

    attemp 4

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -78,7 +78,7 @@ export async function loadHeaderFooter() {
   document.body.appendChild(script);
 
   const script1 = document.createElement('script');
-  script1.src = "/assets/externalServices-e5642083.js";
+  script1.src = "../assets/productSearch.mjs";
   script1.type = "module";
   document.body.appendChild(script1);
 
